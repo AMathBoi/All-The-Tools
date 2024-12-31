9248740ef965fdf3ce6c1978e06ab545baf9d96d
AMathBoi <acomputerman@proton.me>
2024-12-30 21:21:24 -0500

    Initial commit

---
+++ src/main/java/net/amathboi/allthetools/AllTheToolsClient.java
@@ -0,0 +1,10 @@
+package net.amathboi.allthetools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllTheToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
